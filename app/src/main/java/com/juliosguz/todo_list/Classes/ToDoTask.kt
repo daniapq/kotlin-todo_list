a104e1a266358bbfa0cc6faa1c860f529d07790b
Daniapq <daniel.aparicio.q@gmail.com>
2019-02-22 08:26:10 -0400

    Add edit and delete functionality and add firebase database

--- app/src/main/java/com/juliosguz/todo_list/Classes/ToDoTask.kt
+++ app/src/main/java/com/juliosguz/todo_list/Classes/ToDoTask.kt
@@ -1,9 +1,10 @@
-package com.juliosguz.todo_list
+package com.juliosguz.todo_list.Classes
 
 class ToDoTask {
 
     var text: String? = null
     var done: Boolean? = false
+    var objectId: String? = null
 
     companion object Factory {
         fun create(): ToDoTask = ToDoTask()
--- app/src/main/java/com/juliosguz/todo_list/MainActivity.kt
+++ app/src/main/java/com/juliosguz/todo_list/MainActivity.kt
@@ -3,18 +3,34 @@ package com.juliosguz.todo_list
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
+import com.juliosguz.todo_list.Adapters.ToDoTaskAdapter
+import com.juliosguz.todo_list.Classes.ToDoTask
+import com.juliosguz.todo_list.database.Constants
+import com.juliosguz.todo_list.interfaces.ItemRowListener
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemRowListener {
 
-    var toDoTaskList: ArrayList<ToDoTask>? = null
+    lateinit var firebaseDb: DatabaseReference
+    var toDoTaskList: MutableList<ToDoTask>? = null
     lateinit var adapter: ToDoTaskAdapter
     private var listViewItems: ListView? = null
+    var itemListener: ValueEventListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            addDataToList(dataSnapshot)
+        }
+        override fun onCancelled(databaseError: DatabaseError) {
+            Log.w("Database error: ", databaseError.toException())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +40,15 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             showTaskItemDialog()
         }
-        toDoTaskList = ArrayList<ToDoTask>()
-        initializeList()
+        getList()
     }
 
-    private fun initializeList() {
+    fun getList() {
+        firebaseDb = FirebaseDatabase.getInstance().reference
+        toDoTaskList = mutableListOf<ToDoTask>()
         adapter = ToDoTaskAdapter(this, toDoTaskList!!)
         listViewItems!!.setAdapter(adapter)
+        firebaseDb.orderByKey().addListenerForSingleValueEvent(itemListener)
     }
 
     fun showTaskItemDialog() {
@@ -39,15 +57,47 @@ class MainActivity : AppCompatActivity() {
         alert.setTitle("Introduzca su tarea")
         alert.setView(itemEditText)
         alert.setPositiveButton(R.string.save) { _, _ ->
-            val todoItem = ToDoTask.create()
-            todoItem.text = itemEditText.text.toString()
-            todoItem.done = false
-            toDoTaskList?.add(todoItem)
-            adapter.notifyDataSetChanged()
+            val task = ToDoTask.create()
+            task.text = itemEditText.text.toString()
+            task.done = false
+            val newTask = firebaseDb.child(Constants.FIREBASE_COLLECTION).push()
+            task.objectId = newTask.key
+            newTask.setValue(task)
+            getList()
             Toast.makeText(this, "Tarea guardada", Toast.LENGTH_SHORT).show()
         }
         alert.setNegativeButton(R.string.cancel) { _, _ ->   }
         alert.show()
+    }
+
+    fun addDataToList(dataSnapshot: DataSnapshot) {
+        val items = dataSnapshot.children.iterator()
+        if (items.hasNext()) {
+            val toDoListindex = items.next()
+            val itemsIterator = toDoListindex.children.iterator()
+            while (itemsIterator.hasNext()) {
+                val currentItem = itemsIterator.next()
+                Log.d("Hello", currentItem.toString())
+                val todoItem = ToDoTask.create()
+                val map = currentItem.getValue() as HashMap<String, Any>
+                todoItem.objectId = currentItem.key
+                todoItem.done = map.get("done") as Boolean?
+                todoItem.text = map.get("text") as String?
+                toDoTaskList!!.add(todoItem);
+            }
+        }
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun modifyItemState(itemObjectId: String, isDone: Boolean) {
+        val itemReference = firebaseDb.child(Constants.FIREBASE_COLLECTION).child(itemObjectId)
+        itemReference.child("done").setValue(isDone);
+    }
+
+    override fun onItemDelete(itemObjectId: String) {
+        val itemReference = firebaseDb.child(Constants.FIREBASE_COLLECTION).child(itemObjectId)
+        itemReference.removeValue()
+        getList()
     }
 
 }
